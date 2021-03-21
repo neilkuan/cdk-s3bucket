e8b3d54d9e49723359ee7bc3d3e3b773af6cbebb
Neil Guan <guan840912@gmail.com>
2021-03-21 12:49:31 +0800

    add projen-automate-it

--- .projenrc.js
+++ .projenrc.js
@@ -1,9 +1,10 @@
 const { AwsCdkConstructLibrary } = require('projen');
+const { Automation } = require('projen-automate-it');
 
 const PROJECT_NAME = 'cdk-s3bucket-ng';
 const PROJECT_DESCRIPTION = 'cdk-s3bucket-ng is an AWS CDK construct library that provides a drop-in replacement for the Bucket construct with the capability to remove non-empty S3 buckets.';
 const AUTOMATION_TOKEN = 'AUTOMATION_GITHUB_TOKEN';
-const CDK_VERSION = '1.91.0';
+const CDK_VERSION = '1.94.1';
 
 const project = new AwsCdkConstructLibrary({
   name: PROJECT_NAME,
@@ -15,12 +16,10 @@ const project = new AwsCdkConstructLibrary({
   dependabot: false,
   catalog: {
     twitter: 'neil_kuan',
-    announce: false,
+    announce: true,
   },
   defaultReleaseBranch: 'master',
-  projenUpgradeSecret: 'AUTOMATION_GITHUB_TOKEN',
   stability: 'experimental',
-  autoReleaseSchedule: 'never',
   cdkVersion: CDK_VERSION,
   compat: false,
   cdkDependencies: [
@@ -31,54 +30,23 @@ const project = new AwsCdkConstructLibrary({
     '@aws-cdk/core',
     '@aws-cdk/custom-resources',
   ],
+  deps: ['projen-automate-it'],
+  bundledDeps: ['projen-automate-it'],
   python: {
     distName: 'cdk-s3bucket-ng',
     module: 'cdk_s3bucket_ng',
   },
+  rebuildBot: false,
 });
 
-// create a custom projen and yarn upgrade workflow
-workflow = project.github.addWorkflow('ProjenYarnUpgrade');
-
-workflow.on({
-  schedule: [{
-    cron: '11 0 * * *',
-  }], // 0:11am every day
-  workflow_dispatch: {}, // allow manual triggering
+const automation = new Automation(project, {
+  automationToken: AUTOMATION_TOKEN,
 });
-
-workflow.addJobs({
-  upgrade: {
-    'runs-on': 'ubuntu-latest',
-    'steps': [
-      { uses: 'actions/checkout@v2' },
-      {
-        uses: 'actions/setup-node@v1',
-        with: {
-          'node-version': '10.17.0',
-        },
-      },
-      { run: 'yarn upgrade' },
-      { run: 'yarn projen:upgrade' },
-      // submit a PR
-      {
-        name: 'Create Pull Request',
-        uses: 'peter-evans/create-pull-request@v3',
-        with: {
-          'token': '${{ secrets.' + AUTOMATION_TOKEN + ' }}',
-          'commit-message': 'chore: upgrade projen',
-          'branch': 'auto/projen-upgrade',
-          'title': 'chore: upgrade projen and yarn',
-          'body': 'This PR upgrades projen and yarn upgrade to the latest version',
-          'labels': 'auto-merge',
-        },
-      },
-    ],
-  },
-});
+automation.autoApprove();
+automation.autoMerge();
+automation.projenYarnUpgrade();
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'image', 'yarn-error.log', 'coverage'];
 project.gitignore.exclude(...common_exclude);
-
 project.npmignore.exclude(...common_exclude);
 project.synth();
--- API.md
+++ API.md
@@ -35,6 +35,7 @@ new BucketNg(scope: Construct, id: string, props?: BucketProps)
   * **cors** (<code>Array<[CorsRule](#aws-cdk-aws-s3-corsrule)></code>)  The CORS configuration of this bucket. __*Default*__: No CORS configuration.
   * **encryption** (<code>[BucketEncryption](#aws-cdk-aws-s3-bucketencryption)</code>)  The kind of server-side encryption to apply to this bucket. __*Default*__: `Kms` if `encryptionKey` is specified, or `Unencrypted` otherwise.
   * **encryptionKey** (<code>[IKey](#aws-cdk-aws-kms-ikey)</code>)  External KMS key to use for bucket encryption. __*Default*__: If encryption is set to "Kms" and this property is undefined, a new KMS key will be created and associated with this bucket.
+  * **enforceSSL** (<code>boolean</code>)  Enforces SSL for requests. __*Default*__: false
   * **inventories** (<code>Array<[Inventory](#aws-cdk-aws-s3-inventory)></code>)  The inventory configuration of the bucket. __*Default*__: No inventory configuration
   * **lifecycleRules** (<code>Array<[LifecycleRule](#aws-cdk-aws-s3-lifecyclerule)></code>)  Rules that define how Amazon S3 manages objects during their lifetime. __*Default*__: No lifecycle rules.
   * **metrics** (<code>Array<[BucketMetrics](#aws-cdk-aws-s3-bucketmetrics)></code>)  The metrics configuration of this bucket. __*Default*__: No metrics configuration.
