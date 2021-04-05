a67e6af3eb6d3e2b4b234319006eb24251073d25
Neil Guan <guan840912@gmail.com>
2021-04-05 09:25:50 +0800

    update github action

--- .projenrc.js
+++ .projenrc.js
@@ -4,7 +4,7 @@ const { Automation } = require('projen-automate-it');
 const PROJECT_NAME = 'cdk-s3bucket-ng';
 const PROJECT_DESCRIPTION = 'cdk-s3bucket-ng is an AWS CDK construct library that provides a drop-in replacement for the Bucket construct with the capability to remove non-empty S3 buckets.';
 const AUTOMATION_TOKEN = 'AUTOMATION_GITHUB_TOKEN';
-const CDK_VERSION = '1.94.1';
+const CDK_VERSION = '1.96.0';
 
 const project = new AwsCdkConstructLibrary({
   name: PROJECT_NAME,
@@ -42,8 +42,7 @@ const project = new AwsCdkConstructLibrary({
 const automation = new Automation(project, {
   automationToken: AUTOMATION_TOKEN,
 });
-automation.autoApprove();
-automation.autoMerge();
+
 automation.projenYarnUpgrade();
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'image', 'yarn-error.log', 'coverage'];
