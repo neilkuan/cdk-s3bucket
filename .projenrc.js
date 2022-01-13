fe2ea3c7c4da8a44bdeb56f29e19128b4eebbf68
Neil Kuan <guan840912@gmail.com>
2022-01-13 10:04:32 +0800

    chore: deps upgrdae

--- .projenrc.js
+++ .projenrc.js
@@ -14,7 +14,6 @@ const project = new awscdk.AwsCdkConstructLibrary({
     twitter: 'neil_kuan',
     announce: false,
   },
-  defaultReleaseBranch: 'master',
   stability: 'experimental',
   cdkVersion: '2.1.0',
   /**
